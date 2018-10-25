b346aa7650ba6477bbb99979567bf89d5521d6c2
Cameron Beeler <cameron.beeler@gmail.com>
2018-10-25 16:45:36 -0700

    First lambda usage in exercise

--- src/com/cameronbeeler/Main.java
+++ src/com/cameronbeeler/Main.java
@@ -2,7 +2,21 @@ package com.cameronbeeler;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+//        new Thread(new codeToRun()).start();
+        new Thread(()-> System.out.println("Printing from within the lambda expression")).start();
 	// write your code here
+    }
+}
+
+class codeToRun
+implements Runnable
+{
+    @Override
+    public
+    void run()
+    {
+        System.out.println("Printing codeToRun class");
     }
 }
