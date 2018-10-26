7b84fd591fa48a864983c41bda3a4e970b99b248
Cameron Beeler <cameron.beeler@gmail.com>
2018-10-25 17:21:58 -0700

    Lambdas are shorthand for Functional Interfaces, and especially good where single use code exists, like sorting an object on a particular value that wont always be the sort algorithm of choice.

--- src/com/cameronbeeler/Main.java
+++ src/com/cameronbeeler/Main.java
@@ -1,22 +1,70 @@
 package com.cameronbeeler;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main
+{
 
     public static void main(String[] args)
     {
-//        new Thread(new codeToRun()).start();
-        new Thread(()-> System.out.println("Printing from within the lambda expression")).start();
-	// write your code here
+        Employee zoe = new Employee("Zoe", 24);
+        Employee cam = new Employee("Cam", 50);
+        Employee wyatt = new Employee("Wyatt", 14);
+        Employee kim = new Employee("Kim", 39);
+        Employee ani = new Employee("Ani", 16);
+
+        List<Employee> employees = new ArrayList<Employee>();
+        employees.add(zoe);
+	    employees.add(cam);
+        employees.add(wyatt);
+        employees.add(kim);
+        employees.add(ani);
+
+        Collections.sort(employees, (Employee employee1, Employee employee2)->
+                employee1.getName().compareTo(employee2.getName()));
+
+        for (Employee employee : employees)
+        {
+            System.out.println(employee.getName());
+        }
     }
 }
 
-class codeToRun
-implements Runnable
+class Employee
 {
-    @Override
-    public
-    void run()
+    private String name;
+    private int age;
+
+    public Employee(String name, int age)
     {
-        System.out.println("Printing codeToRun class");
+        this.name = name;
+        this.age = age;
+    }
+
+    public
+    String getName()
+    {
+        return name;
+    }
+
+    public
+    void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public
+    int getAge()
+    {
+        return age;
+    }
+
+    public
+    void setAge(int age)
+    {
+        this.age = age;
     }
 }
